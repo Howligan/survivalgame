92e8c817236214fbc51c3277a5ce20512a384f96
Howligan <redrebel62@gmail.com>
2017-03-20 08:51:40 -0400

    3-20-17
    Procedural Landmass Generation is working
    Tree normals fixed
    Still need to figure out spawning trees

---
+++ SurvivalGameCurrent/SurvivalGame/Assets/Scripts/SetMeshCollider.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetMeshCollider : MonoBehaviour {
+
+   public MeshCollider meshCol;
+   public MeshFilter meshFilt;
+	// Use this for initialization
+	void Start () {
+        meshCol = gameObject.GetComponent<MeshCollider>();
+        meshFilt = gameObject.GetComponent<MeshFilter>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    public void SetMesh()
+    {
+       // meshCol.sharedMesh = meshFilt.mesh;
+
+    }
+}
